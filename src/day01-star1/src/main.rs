bab8b7c5ca9389350b727d3081914372eda1e837
foognostic <seth@foognostic.sh>
2021-04-30 19:50:38 +0000

    Super gross, but it works

--- src/day01-star1/src/main.rs
+++ src/day01-star1/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+mod test_data;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let lines = test_data::read_test_data()?;
+
+    for outer in &lines {
+        for inner in &lines {
+            if 2020 == inner + outer {
+                println!(
+                    "inner: {}, outer: {}, product: {}",
+                    inner,
+                    outer,
+                    inner * outer
+                );
+                break;
+            }
+        }
+    }
+    Ok(())
 }
