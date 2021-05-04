e86c2c8ed7dd41efda50b886544aee894ff20ecc
foognostic <seth@foognostic.sh>
2021-05-04 00:41:05 +0000

    Wonder about that usize and maybe a panic?

--- src/day02-star1/src/main.rs
+++ src/day02-star1/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+mod test_data;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = test_data::read_test_data()?;
+    let mut valid = 0;
+    let mut invalid = 0;
+
+    for line in input {
+        let pieces: Vec<&str> = line.split(" ").collect();
+
+        let range = pieces[0];
+        let range_pieces: Vec<&str> = range.split("-").collect();
+        let open:usize = range_pieces[0].parse().unwrap();
+        let close:usize = range_pieces[1].parse().unwrap();
+
+        let pat = pieces[1].replace(":", "");
+
+        let password = pieces[2];
+
+        let hits: Vec<&str> = password.matches(&pat).collect();
+        if hits.len() >= open && hits.len() <= close {
+            valid += 1;
+        } else {
+            invalid += 1;
+        }
+    }
+
+    println!("total: {}, invalid: {}, valid: {}", invalid + valid, invalid, valid);
+
+    Ok(())
 }
