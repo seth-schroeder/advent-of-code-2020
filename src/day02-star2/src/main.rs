bf430df12bc49a7fdb469c5329cd81704335d98a
foognostic <seth@foognostic.sh>
2021-05-04 01:55:19 +0000

    Avoided indexing into a string
    + instead of a big complex loop, make a histogram and check it
      in small pieces
    + really working the C programmer out of me

--- src/day02-star2/src/main.rs
+++ src/day02-star2/src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::error::Error;
+
+mod test_data;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = test_data::read_test_data()?;
+    let mut valid = 0;
+    let mut invalid = 0;
+
+    for line in input {
+        let pieces: Vec<&str> = line.split(' ').collect();
+
+        let range = pieces[0];
+        let range_pieces: Vec<&str> = range.split('-').collect();
+        let open: usize = range_pieces[0].parse().unwrap();
+        let close: usize = range_pieces[1].parse().unwrap();
+
+        let password = pieces[2];
+        let mut occurs = HashMap::new();
+
+        for (i, c) in password.char_indices() {
+            let mut s = String::new();
+            s.push(c);
+            occurs.insert(i + 1, s);
+        }
+
+        let pat = pieces[1].replace(":", "");
+
+        let first_match = match occurs.get(&open) {
+            Some(s) => *s == pat,
+            None => false,
+        };
+
+        let last_match = match occurs.get(&close) {
+            Some(s) => *s == pat,
+            None => false,
+        };
+
+        if (first_match && last_match) || !(first_match || last_match) {
+            invalid += 1;
+        } else {
+            valid += 1
+        }
+    }
+
+    println!(
+        "total: {}, invalid: {}, valid: {}",
+        invalid + valid,
+        invalid,
+        valid
+    );
+
+    Ok(())
 }
