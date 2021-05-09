74b7b7ded5e1c991f34bce188ad89b6bb8244983
foognostic <seth@foognostic.sh>
2021-05-09 02:52:49 +0000

    I'm not a fan of filter and double references
    + it's really hard to get away from `for` loops

--- src/day04-star1/src/main.rs
+++ src/day04-star1/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::error::Error;
+
+mod test_data;
+
+fn valid_hash(h: &HashMap<String, String>) -> bool {
+    let required_keys = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+    for k in required_keys {
+        if !h.contains_key(k) {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let hashes = test_data::data_to_hashes(&test_data::read_test_data()?);
+    let mut valid = 0;
+
+    for h in hashes {
+        if valid_hash(&h) {
+            valid += 1;
+        }
+    }
+
+    println!("Yup I found {} valid items", valid);
+
+    Ok(())
 }
