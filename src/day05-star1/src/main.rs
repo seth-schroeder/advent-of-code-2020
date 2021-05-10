404e6ed8a60e5a0146daff9fa4e2cde9e97ff46e
foognostic <seth@foognostic.sh>
2021-05-10 03:11:42 +0000

    Before refactoring

--- src/day05-star1/src/main.rs
+++ src/day05-star1/src/main.rs
@@ -18,17 +18,42 @@ fn half_of(floor: u32, ceiling: u32) -> u32 {
     }
 }
 
-fn bottom_half(floor: u32, ceiling: u32) -> (u32, u32) {
+fn lower_half(floor: u32, ceiling: u32) -> (u32, u32) {
     (floor, floor + half_of(floor, ceiling) - 1)
 }
 
-fn top_half(floor: u32, ceiling: u32) -> (u32, u32) {
+fn upper_half(floor: u32, ceiling: u32) -> (u32, u32) {
     (ceiling - half_of(floor, ceiling) + 1, ceiling)
+}
+
+
+// probably a good scenario for reduce
+fn walk(s: &str) -> u32 {
+    let mut range = (0, 127);
+
+    for c in s.chars() {
+        if c == 'B' {
+            range = upper_half(range.0, range.1);
+        } else if c == 'F' {
+            range = lower_half(range.0, range.1);
+        }
+    }
+
+    if range.0 != range.1 {
+        panic!("the range is wrong: {:?}", range);
+    }
+
+    range.0
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_walk() {
+        assert_eq!(walk("FBFBBFF"), 44);
+    }
 
     #[test]
     fn test_half_of() {
@@ -47,13 +72,15 @@ mod tests {
     }
 
     #[test]
-    fn test_bottom_half() {
-        assert_eq!(bottom_half(0, 127), (0, 63));
+    fn test_lower_half() {
+        assert_eq!(lower_half(0, 127), (0, 63));
+        assert_eq!(lower_half(44, 45), (44, 44));
     }
 
     #[test]
-    fn test_top_half() {
-        assert_eq!(top_half(0, 7), (4, 7));
-        assert_eq!(top_half(0, 63), (32, 63));
+    fn test_upper_half() {
+        assert_eq!(upper_half(0, 7), (4, 7));
+        assert_eq!(upper_half(0, 63), (32, 63));
+        assert_eq!(upper_half(44, 45), (45, 45));
     }
 }
