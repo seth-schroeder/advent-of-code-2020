9502d86e7047dd1d677b80fa5ad60054b51e430c
foognostic <seth@foognostic.sh>
2021-05-11 02:24:47 +0000

    Yay! I got to use reduce
    + fold seems better at first glance

--- src/day06-star1/src/main.rs
+++ src/day06-star1/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+use std::error::Error;
+
+mod test_data;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let lines = test_data::read_test_data()?;
+    let groups = lines_to_groups(&lines);
+
+    let sum = groups.iter().fold(0, |memo, group| memo + group.len());
+
+    println!("The total # of questions answered was {}", sum);
+
+    Ok(())
+}
+
+fn lines_to_groups(v: &[String]) -> Vec<HashSet<char>> {
+    let mut output = Vec::new();
+    let mut bucket: HashSet<char> = HashSet::new();
+
+    for line in v {
+        if line.is_empty() {
+            output.push(bucket.clone());
+            bucket.clear();
+        } else {
+            for c in line.chars() {
+                bucket.insert(c);
+            }
+        }
+    }
+
+    if !bucket.is_empty() {
+        output.push(bucket);
+    }
+
+    output
 }
