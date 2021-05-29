12f67d6628c37582bb2b40400331f411754c6ba5
foognostic <seth@foognostic.sh>
2021-05-29 17:23:12 +0000

    kill it with fire, read from stdin

--- src/day10-star2/src/lib.rs
+++ src/day10-star2/src/lib.rs
@@ -5,11 +5,75 @@ use num_integer;
 use std::fs;
 use std::io;
 
-pub fn run() {
-    // let mut joltages = read_test_data("day10-star1/largest.txt").unwrap();
-}
+type Adapters = Vec<u32>;
+type Groups = Vec<u32>;
 
 const WINDOW: u32 = 3;
+
+pub fn run() {
+    let v = prompt_for_test_data();
+    let v = prep_data(&v);
+    println!("Working with {:?}", v);
+
+    let groups = group_it_up(&v);
+    println!("\n seeing these groups: {:#?}, {}", groups, groups.len());
+}
+
+fn prep_data(data: &Adapters) -> Adapters {
+    let mut clean_data = Adapters::with_capacity(data.len() + 2);
+
+    clean_data.clone_from(data);
+    clean_data.push(0);
+    clean_data.sort_unstable();
+
+    clean_data.push(clean_data.iter().max().unwrap() + 3);
+
+    clean_data
+}
+
+fn group_it_up(data: &Adapters) -> Groups {
+    let mut g = Groups::new();
+
+    let mut group_min_val = 0;
+    let mut last_val = 0;
+    let mut idx = 0;
+
+    loop {
+        if idx >= data.len() {
+            break;
+        }
+
+        let current = *data.get(idx).unwrap();
+        println!(
+            "cur: {}, gmv: {}, lv: {}, idx: {}, #groups: {}",
+            current,
+            group_min_val,
+            last_val,
+            idx,
+            g.len()
+        );
+
+        if current == last_val + 1 {
+            last_val = current;
+        } else {
+            println!("starting a group at {}", idx);
+            if idx > 0 {
+                println!("not the first group");
+                let group_size = last_val - group_min_val + 1;
+                g.push(group_size);
+            }
+            group_min_val = current;
+            last_val = current;
+        }
+
+        idx += 1
+    }
+
+    // finish up the trailing group
+    g.push(last_val - group_min_val + 1);
+
+    g
+}
 
 // now I'm really caffeinated and the dog needs to walk soon
 fn fuckit_too_early_comma_just_count_the_breakables(adapters: &[u32]) -> u32 {
@@ -25,7 +89,10 @@ fn fuckit_too_early_comma_just_count_the_breakables(adapters: &[u32]) -> u32 {
                 if let Some(next_adapter) = adapters.get(i + 1) {
                     let gap = next_adapter - previous_adapter;
                     if gap < WINDOW {
-                        eprintln!("{} is breakable: {} <- {}:{}:{}", current_adapter, gap, previous_adapter, current_adapter, next_adapter);
+                        eprintln!(
+                            "{} is breakable: {} <- {}:{}:{}",
+                            current_adapter, gap, previous_adapter, current_adapter, next_adapter
+                        );
                         breakable += 1;
                     }
                 }
@@ -62,7 +129,6 @@ mod tests {
         let input = vec![];
         assert_eq!(0, fuckit_too_early_comma_just_count_the_breakables(&input));
 
-
         let mut input = read_test_data("day10-star1/smallest.txt").unwrap();
         input.sort_unstable();
         input.insert(0, 0);
@@ -72,7 +138,7 @@ mod tests {
 
         assert_eq!(8, sum_the_mfn_coefficients(output));
 
-        let mut input = read_test_data("day10-star1/medium.txt").unwrap();
+        let mut input = read_test_data("day10-star1/exp.txt").unwrap();
         input.sort_unstable();
         input.insert(0, 0);
         input.push(input.last().unwrap() + 3);
@@ -96,4 +162,24 @@ fn read_test_data(relative_file_name: &str) -> Result<Vec<u32>, io::Error> {
     }
 
     Ok(mv)
+}
+
+fn prompt_for_test_data() -> Vec<u32> {
+    let mut v = Vec::new();
+
+    println!("> (empty line when done)");
+
+    loop {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).expect("need more data pls");
+        let s = s.trim();
+
+        if s.trim().is_empty() {
+            break;
+        } else {
+            v.push(s.parse().unwrap())
+        }
+    }
+
+    v
 }
