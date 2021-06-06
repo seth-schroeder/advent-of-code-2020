ea7d395f0ca5eafb7c27ef0c2321e05eed4cb40a
foognostic <seth@foognostic.sh>
2021-06-06 12:24:02 +0000

    New day, new tests

--- src/day11-star2/src/cartesianarray2d.rs
+++ src/day11-star2/src/cartesianarray2d.rs
@@ -33,17 +33,43 @@ impl<T: Clone> IndexMut<(usize, usize)> for Cartesian2DArray<T> {
 mod tests {
     use super::*;
 
+    fn data() -> Vec<Vec<char>> {
+        vec![
+            vec!['a', 'b', 'c'],
+            vec!['d', 'e', 'f'],
+            vec!['g', 'h', 'i'],
+        ]
+    }
+
     #[test]
-    fn test_origin() {
-        let data = vec![vec!['a', 'b', 'c'], vec!['d', 'e', 'f'], vec!['g', 'h', 'i']];
-        let arr2d = Array2D::from_rows(&data);
+    fn test_write() {
+        let arr2d = Array2D::from_rows(&data());
+
+        assert_eq!('a', arr2d[(0, 0)]);
+
+        let mut carr2d = Cartesian2DArray {
+            backing_store: arr2d,
+        };
+
+        assert_eq!('a', carr2d[(0, 2)]);
+        carr2d[(0, 2)] = 'z';
+        assert_eq!('z', carr2d[(0, 2)]);
+
+        assert_eq!('z', carr2d.backing_store[(0, 0)]);
+    }
+
+    #[test]
+    fn test_read() {
+        let arr2d = Array2D::from_rows(&data());
 
         assert_eq!('a', arr2d[(0, 0)]);
         assert_eq!('b', arr2d[(0, 1)]);
         assert_eq!('g', arr2d[(2, 0)]);
         assert_eq!('h', arr2d[(2, 1)]);
 
-        let carr2d = Cartesian2DArray { backing_store: arr2d };
+        let carr2d = Cartesian2DArray {
+            backing_store: arr2d,
+        };
         assert_eq!('a', carr2d[(0, 2)]);
         assert_eq!('g', carr2d[(0, 0)]);
         assert_eq!('i', carr2d[(2, 0)]);
