776fe60cea19ba97703fd28bdb7358f9695c6568
foognostic <seth@foognostic.sh>
2021-06-07 04:06:32 +0000

    Less abstraction being more successful :-(
    + maybe I was overdoing it before, but this is a small, well defined problem

--- src/day11-star2/src/lib.rs
+++ src/day11-star2/src/lib.rs
@@ -1,28 +1,137 @@
-mod cartesianarray2d;
-mod vector;
-
-use array2d::Array2D;
-use cartesianarray2d::CartesianArray2D;
+use std::collections::BTreeMap;
 use std::convert::TryFrom;
 use std::fmt;
 use std::fs;
 use std::io;
 
 pub fn run() {
-    let first_round = read_test_data("day11-star1/smallest.txt").unwrap();
-    println!("{}", roll_tape(&first_round),);
+    let input_data = read_test_data("day11-star1/actual.txt").unwrap();
+    let mut last_round = input_data;
+    let mut frames = 0;
+
+    loop {
+        let next_round = evolve(&last_round);
+        frames += 1;
+
+        if next_round == last_round {
+            break;
+        }
+
+        last_round = next_round;
+    }
+
+    println!(
+        "{} neighbors, it took {} frames, finished with \n{}",
+        count_neighbors(&last_round),
+        frames,
+        textify_board(&last_round)
+    );
 }
 
-const MAX_NEIGHBORS: u8 = 4;
+fn count_neighbors(board: &Board) -> u32 {
+    let mut headcount = 0;
+
+    for xh in board.values() {
+        for seat in xh.values() {
+            if let Seat::Occupied = seat {
+                headcount += 1
+            }
+        }
+    }
+
+    headcount
+}
+
+fn scan_for_neighbors(board: &Board, x: usize, y: usize) -> u32 {
+    let compass_rose: Vec<(i8, i8)> = vec![
+        (1, 0),
+        (1, 1),
+        (0, 1),
+        (-1, 1),
+        (-1, 0),
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+    ];
+    let ix = i8::try_from(x).unwrap();
+    let iy = i8::try_from(y).unwrap();
+    let mut neighbors = 0;
+
+    for slope in compass_rose {
+        let (y_slope, x_slope) = slope;
+        let mut at_y = iy + y_slope;
+        let mut at_x = ix + x_slope;
+
+        loop {
+            if at_y < 0 || at_x < 0 {
+                break;
+            }
+
+            match board.get(&usize::try_from(at_y).unwrap()) {
+                None => break,
+                Some(xh) => match xh.get(&usize::try_from(at_x).unwrap()) {
+                    None => break,
+                    Some(seat) => {
+                        match seat {
+                            Seat::Floor => (),
+                            Seat::Occupied => {
+                                neighbors += 1;
+                                break;
+                            }
+                            Seat::Empty => break,
+                        }
+                    }
+                },
+            }
+
+            at_y += y_slope;
+            at_x += x_slope;
+        }
+    }
+
+    neighbors
+}
+
+fn evolve(board: &Board) -> Board {
+    let mut b = board.clone();
+
+    for (y_index, y_hash) in board {
+        let by_hash = b.get_mut(y_index).unwrap();
+
+        for (x_index, seat) in y_hash {
+            let new_seat = match seat {
+                Seat::Floor => Seat::Floor,
+                Seat::Empty => {
+                    let neighbors = scan_for_neighbors(board, *x_index, *y_index);
+                    if neighbors == 0 {
+                        Seat::Occupied
+                    } else {
+                        Seat::Empty
+                    }
+                }
+                Seat::Occupied => {
+                    let neighbors = scan_for_neighbors(board, *x_index, *y_index);
+                    if neighbors >= 5 {
+                        Seat::Empty
+                    } else {
+                        Seat::Occupied
+                    }
+                }
+            };
+
+            by_hash.insert(*x_index, new_seat);
+        }
+    }
+
+    b
+}
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Seat {
     Empty,
-    Full,
+    Occupied,
     Floor,
 }
-
-type SeatingArea = CartesianArray2D<Seat>;
 
 impl fmt::Display for Seat {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -30,7 +139,7 @@ impl fmt::Display for Seat {
             f,
             "{}",
             match self {
-                Seat::Full => '#',
+                Seat::Occupied => '#',
                 Seat::Empty => 'L',
                 Seat::Floor => '.',
             }
@@ -38,231 +147,51 @@ impl fmt::Display for Seat {
     }
 }
 
-impl Seat {
-    // moving a new Vec out seems really expensive
-    fn adjacent_indices(x_y: (usize, usize), seats: &SeatingArea) -> Vec<(usize, usize)> {
-        let mut v = Vec::new();
-
-        // "no need to unit test boundary conditions when mixing indices and counts"
-        // me, several hours ago.
-        let (x, y) = x_y;
-        let x_min = if x == 0 { x } else { x - 1 };
-        let x_max = seats.num_rows().min(x + 2);
-        let y_min = if y == 0 { y } else { y - 1 };
-        let y_max = seats.num_columns().min(y + 2);
-
-        // eprintln!(
-        //     "aji {}..{}..{}, {}..{}..{}",
-        //     x_min, x, x_max, y_min, y, y_max
-        // );
-
-        // hello there, doubleplus extra time in Purgatory
-        for x_ in x_min..x_max {
-            for y_ in y_min..y_max {
-                if x_y == (x_, y_) {
-                    continue;
-                }
-
-                // eprintln!("aji inserting {:?} for {:?}", (x_, y_), x_y);
-                v.push((x_, y_));
-            }
-        }
-
-        v
-    }
-}
-
-fn textify_seating_area(seats: &SeatingArea) -> String {
+fn textify_board(board: &Board) -> String {
     let mut s = Vec::new();
-    let mut x = seats.num_columns() - 1;
 
-    loop {
-        let mut r = Vec::new();
-        let mut y = 0;
-        while y < seats.num_columns() {
-            let seat = seats[(x, y)];
-            r.push(format!("{}", seat));
-            y += 1
+    for yh in board.values() {
+        let mut v = Vec::new();
+        for seat in yh.values() {
+            v.push(format!("{}", seat))
         }
-        s.push(r.join(""));
-        if x == 0 {
-            break
-        }
-        x -= 1;
+        s.push(v.join(""));
     }
 
     s.join("\n")
 }
 
-// hmm the Hollywood metaphors are wan tonight
-fn roll_tape(seats: &SeatingArea) -> u32 {
-    let mut frames = 0;
+type BoardIndex = usize;
+type Board = BTreeMap<BoardIndex, BTreeMap<BoardIndex, Seat>>;
 
-    // pushing and popping a one element vector is a 1am workaround for juggling ownership and borrowing in a loop
-    let mut v = vec![];
-
-    loop {
-        if frames == 2 {
-            break;
-        }
-
-        frames += 1;
-
-        if v.is_empty() {
-            v.push(lights_camera_action(seats));
-            continue;
-        }
-
-        let last_area = v.first().unwrap();
-        let this_area = lights_camera_action(&last_area);
-
-        if *last_area == this_area {
-            println!("\n");
-            break;
-        }
-
-        print!(".");
-        v.pop();
-        v.push(this_area);
-    }
-
-    eprintln!("It took {} frames", frames);
-    count_occupants(&v.pop().unwrap())
-}
-
-fn lights_camera_action(seats: &SeatingArea) -> SeatingArea {
-    let mut next_round = seats.clone();
-    let mut x = 0;
-    let mut y = 0;
-
-    // eprintln!("{:#?}", seats);
-    // eprintln!("===============================================");
-    // eprintln!("before\n{}", textify_seating_area(seats));
-
-    while x < seats.num_rows() {
-        while y < seats.num_columns() {
-            let x_y = (x, y);
-            let neighbors = count_neighbors(x_y, seats);
-            eprintln!("found {} neighbors for {:?}", neighbors, x_y);
-
-            let new_seat = match seats[x_y] {
-                Seat::Full => {
-                    if neighbors <= MAX_NEIGHBORS {
-                        Seat::Full
-                    } else {
-                        Seat::Empty
-                    }
-                }
-                Seat::Empty => {
-                    if neighbors == 0 {
-                        Seat::Full
-                    } else {
-                        Seat::Empty
-                    }
-                }
-                Seat::Floor => Seat::Floor,
-            };
-
-            let seat = seats[x_y];
-            if seat == new_seat {
-                println!("same seat at {} -> {:?}", seat, x_y)
-            } else {
-                eprintln!("NEW  seat at {:?} from {} to {}", x_y, seats[x_y], new_seat);
-            }
-            next_round[x_y] = new_seat;
-            println!("\n\n{}\n", textify_seating_area(&next_round));
-
-            y += 1;
-        }
-        y = 0;
-        x += 1;
-    }
-
-    eprintln!("\nafter\n{}", textify_seating_area(&next_round));
-    next_round
-}
-
-fn count_occupants(seats: &SeatingArea) -> u32 {
-    let mut headcount = 0;
-    let mut x = 0;
-    let mut y = 0;
-
-    while x < seats.num_columns() {
-        while y < seats.num_rows() {
-            if let Seat::Full = seats[(x, y)] {
-                headcount += 1;
-            }
-            y += 1;
-        }
-        x += 1;
-    }
-
-    headcount
-}
-
-fn count_neighbors(x_y: (usize, usize), seats: &SeatingArea) -> u8 {
-    let mut headcount = 0;
-    let (x, y) = x_y;
-    let point = vector::Point {
-        x: i32::try_from(x).unwrap(),
-        y: i32::try_from(y).unwrap(),
-    };
-    let grid = vector::Grid {
-        height: seats.num_rows(),
-        width: seats.num_columns(),
-    };
-
-    for orientation in vector::Compass::rose() {
-        let path = grid.path(&point, &orientation);
-        let mut i = 0;
-        eprintln!("{:?} from {:?} path = {:#?}", orientation, point, path);
-        while i < path.len() {
-            let next_point = path.get(i).unwrap();
-            let next_x_y = (
-                usize::try_from(next_point.x).unwrap(),
-                usize::try_from(next_point.y).unwrap(),
-            );
-            if seats[next_x_y] == Seat::Full {
-                eprintln!("found someone at {:?}", next_x_y);
-                headcount += 1;
-                break;
-            }
-            i += 1;
-        }
-    }
-
-    headcount
-}
-
-// okay it's getting closer to time to make this into a local crate or something
-fn read_test_data(relative_file_name: &str) -> Result<CartesianArray2D<Seat>, io::Error> {
+fn read_test_data(relative_file_name: &str) -> Result<Board, io::Error> {
     let path = fs::canonicalize(format!("../../input-data/{}", relative_file_name))?;
     let s = fs::read_to_string(path)?;
 
-    let mut mv = Vec::new();
+    let mut yh = Board::new();
+    let mut y = 0;
 
     for line in s.lines() {
-        let mut v = Vec::new();
+        let mut xh = BTreeMap::new();
+        let mut x = 0;
+
         for c in line.trim().chars() {
-            v.push(match c {
-                '#' => Seat::Full,
-                'L' => Seat::Empty,
-                '.' => Seat::Floor,
-                _ => panic!("bad data!"),
-            });
+            xh.insert(
+                x,
+                match c {
+                    '#' => Seat::Occupied,
+                    'L' => Seat::Empty,
+                    '.' => Seat::Floor,
+                    _ => panic!("bad data!"),
+                },
+            );
+            x += 1;
         }
-        mv.push(v);
+
+        yh.insert(y, xh);
+        y += 1;
     }
 
-    Ok(CartesianArray2D {
-        backing_store: Array2D::from_rows(&mv),
-    })
+    Ok(yh)
 }
 
-////////////////////////////////////////////////////////////////////////////////
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-// }
--- src/day11-star2/src/main.rs
+++ src/day11-star2/src/main.rs
@@ -1,7 +1,3 @@
-#[cfg(test)]
-#[macro_use]
-extern crate assert_matches;
-
 fn main() {
     day11_star2::run();
 }
