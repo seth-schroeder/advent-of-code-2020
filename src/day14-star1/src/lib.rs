68bcb2c59d9ad21c169903a1664f6cfea0766478
foognostic <seth@foognostic.sh>
2021-06-17 01:57:12 +0000

    Living the sin of refactoring WIP code
    + don't tell Mom

--- src/day14-star1/src/lib.rs
+++ src/day14-star1/src/lib.rs
@@ -1,4 +1,5 @@
 mod compute;
+mod mask;
 
 pub fn run() {
     let v: u64 = 2 << 35;
---
+++ src/day14-star1/src/mask.rs
@@ -0,0 +1,114 @@
+use crate::compute;
+use std::collections::BTreeMap;
+use std::convert::TryFrom;
+use std::fmt;
+
+struct Mask {
+    mask: BTreeMap<compute::Address, Operation>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum Operation {
+    One,
+    Zero,
+    AsIs,
+}
+
+impl TryFrom<char> for Operation {
+    type Error = &'static str;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            '0' => Ok(Operation::Zero),
+            '1' => Ok(Operation::One),
+            'X' => Ok(Operation::AsIs),
+            _ => Err("unrecognized char!"),
+        }
+    }
+}
+
+impl fmt::Display for Operation {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Operation::One => '1',
+                Operation::Zero => '0',
+                Operation::AsIs => 'X',
+            }
+        )
+    }
+}
+
+impl Mask {
+    fn new() -> Self {
+        Mask {
+            mask: BTreeMap::new(),
+        }
+    }
+
+    fn load(s: &str) -> Result<Mask, &'static str> {
+        let mut m = Mask::new();
+        for (i, c) in s.chars().rev().enumerate() {
+            m.mask.insert(i as u64, Operation::try_from(c)?);
+        }
+        Ok(m)
+    }
+}
+
+impl fmt::Display for Mask {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.mask
+                .values()
+                .map(|v| v.to_string())
+                .rev()
+                .collect::<Vec<String>>()
+                .join("")
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_try_from_char() {
+        // went back and forth on assert_matches... standing down for now
+        assert_eq!(Ok(Operation::One), Operation::try_from('1'));
+        assert_eq!(Ok(Operation::Zero), Operation::try_from('0'));
+        assert_eq!(Ok(Operation::AsIs), Operation::try_from('X'));
+        assert_eq!(Err("unrecognized char!"), Operation::try_from('?'));
+    }
+
+    #[test]
+    fn test_mask_value() {
+        assert_eq!("0", format!("{}", Operation::Zero));
+        assert_eq!("1", format!("{}", Operation::One));
+        assert_eq!("X", format!("{}", Operation::AsIs));
+    }
+
+    #[test]
+    fn test_mask_load() {
+        let m = Mask::load("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X").unwrap();
+        assert_eq!(m.mask.get(&0), Some(&Operation::AsIs));
+        assert_eq!(m.mask.get(&1), Some(&Operation::Zero));
+        assert_eq!(m.mask.get(&2), Some(&Operation::AsIs));
+        assert_eq!(m.mask.get(&3), Some(&Operation::AsIs));
+        assert_eq!(m.mask.get(&4), Some(&Operation::AsIs));
+        assert_eq!(m.mask.get(&5), Some(&Operation::AsIs));
+        assert_eq!(m.mask.get(&6), Some(&Operation::One));
+        assert_eq!(m.mask.get(&7), Some(&Operation::AsIs));
+    }
+
+    #[test]
+    fn test_mask_to_string() {
+        let s = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
+        let m = Mask::load(s).unwrap();
+        assert_eq!(s, m.to_string());
+    }
+}
