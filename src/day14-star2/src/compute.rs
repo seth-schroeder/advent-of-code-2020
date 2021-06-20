baf2f3ab3a2f1167f40a9f6bca72ef5d689713e0
foognostic <seth@foognostic.sh>
2021-06-20 22:48:44 +0000

    Just a little bit at a time
    + no one will read this
    + but this was a Dad joke and today is Fathers' Day :D

--- src/day14-star2/src/compute.rs
+++ src/day14-star2/src/compute.rs
@@ -24,6 +24,41 @@ pub fn binary_permutation_range(v: &[Value]) -> ops::Range<Value> {
     0..binary_permutations(v.len() as Value)
 }
 
+pub fn value_to_bit_array(v: Value, num_bits: Value) -> Vec<u8> {
+    // eeeeeeeeeeek this smells
+    if v > 2_u32.pow(num_bits as u32).into() {
+        panic!("bit underflow!");
+    }
+
+    let mut ba = Vec::with_capacity(num_bits as usize);
+    let mut work = v as u8;
+    let mut i = num_bits;
+
+    while i > 0 {
+        let bit = work & 1;
+        ba.insert(0, bit);
+        work >>= 1;
+        i -= 1;
+    }
+
+    ba
+}
+
+pub fn float_bit_array(bits: &[u8], floaters: &[Value]) -> HashMap<Value, Value> {
+    if bits.len() != floaters.len() {
+        panic!("yo that's not right");
+    }
+
+    let mut h = HashMap::new();
+
+    for (i, float) in floaters.iter().enumerate() {
+        let bit = bits[i] as Value;
+        h.insert(*float, bit);
+    }
+
+    h
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,4 +88,28 @@ mod tests {
         assert_eq!(0..8, binary_permutation_range(&vec![0, 1, 2]));
         assert_eq!(0..16, binary_permutation_range(&vec![0, 1, 2, 3]));
     }
+
+    #[test]
+    fn test_value_to_bit_array() {
+        assert_eq!(vec![0], value_to_bit_array(0, 1));
+        assert_eq!(vec![0, 0], value_to_bit_array(0, 2));
+        assert_eq!(vec![0, 0, 0], value_to_bit_array(0, 3));
+        assert_eq!(vec![1], value_to_bit_array(1, 1));
+        assert_eq!(vec![0, 1], value_to_bit_array(1, 2));
+        assert_eq!(vec![0, 0, 1], value_to_bit_array(1, 3));
+        assert_eq!(vec![1, 1, 1], value_to_bit_array(7, 3));
+        assert_eq!(vec![1, 0, 1], value_to_bit_array(5, 3));
+    }
+
+    #[test]
+    fn test_float_bit_array() {
+        let h = float_bit_array(&vec![0, 1], &vec![5, 0]);
+        assert_eq!(&0, h.get(&5).unwrap());
+        assert_eq!(None, h.get(&4));
+        assert_eq!(None, h.get(&3));
+        assert_eq!(None, h.get(&2));
+        assert_eq!(None, h.get(&1));
+        assert_eq!(&1, h.get(&0).unwrap());
+    }
+
 }
