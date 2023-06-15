5d9016fd7abfa4fc41bf2a71556f159953e54c69
Seth Schroeder <seth.schroeder@gmail.com>
2023-06-15 11:27:41 -0400

    It just worked for day11 star 1. HUZZAH.

--- src/day11-star1/src/lib.rs
+++ src/day11-star1/src/lib.rs
@@ -1,5 +1,8 @@
+use lucio::day::eleven::*;
+
 pub fn run() {
     let data = lucio::get_input_data(11).unwrap();
-    let first_round = lucio::day::eleven::parse_input_data(&data).unwrap();
-    println!("{}", lucio::day::eleven::roll_tape(&first_round));
+    let first_round = seat::parse_input_data(&data).unwrap();
+    let area = area::Area { max_neighbors: 3, scope: area::ScopeToScan::Adjacent };
+    println!("{}", do_the_thing(&area, first_round));
 }
