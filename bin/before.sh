ac8063c1f6da4ce13231a90a63d17dbc6df4db6b
Seth Schroeder <seth.schroeder@gmail.com>
2023-08-07 10:48:53 -0400

    Start preparing the helper script for go

--- bin/before.sh
+++ bin/before.sh
@@ -10,7 +10,7 @@ if [[ "${_day:-um}" = um ]] || [[ "${_star:-um}" = um ]]; then
     exit 1
 fi
 
-branch_name="day${_day}-star${_star}"
+branch_name="go/day${_day}-star${_star}"
 echo $branch_name
 
 git stash create "before ${branch_name}"
@@ -19,8 +19,12 @@ git checkout -b "${branch_name}"
 mkdir -p "input-data/${branch_name}"
 mkdir -p src
 cd src
-cargo new "${branch_name}" --bin --vcs none
-git add "${branch_name}"
+# TODO: what is the golang version of this?
+
+mkdir -p "${branch_name}"
+cd "${branch_name}"
+go mod init "github.com/seth-schroeder/advent-of-code-2020/${_day}/${_star}"
+git add .
 
 git commit -m "Created branch for ${branch_name}"
 cd "${branch_name}"
